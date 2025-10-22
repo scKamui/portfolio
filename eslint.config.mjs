6082eb661498e06abd46c02d4f99bccee373aa71
Sami Chauhan <chauhansam02@gmail.com>
2025-10-21 18:16:24 -0700

    Disable unescaped entities rule in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import next from "eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+/** @type {import("eslint").Linter.Config[]} */
+export default [
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["**/node_modules/**"],
+  },
+  ...next(),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
   },
 ];
 
-export default eslintConfig;
